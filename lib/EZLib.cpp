487b3e6eafe46c6652cc0f7034aa4216e90d6e92
ppk <ath455756@gmail.com>
2021-07-21 17:04:31 +0630

    use C++17 now

--- lib/EZLib.cpp
+++ lib/EZLib.cpp
@@ -34,15 +34,18 @@ void makedir(std::string dirname){
     MKDIR(dirname);
 }
 
-void download(std::string url, std::string output){
+void download(std::string url, std::string output, bool re){
     curl_global_init(CURL_GLOBAL_ALL);
 
     CURL *handle = curl_easy_init();
-    FILE *fp = fopen(output.c_str(), "wb");
+    FILE *fp = fopen((curdir+'/'+output).c_str(), "wb");
     std::cout << output << '\n';
 
     curl_easy_setopt(handle, CURLOPT_URL, url.c_str());
     curl_easy_setopt(handle, CURLOPT_NOPROGRESS, 0L);
+    if (re){
+        curl_easy_setopt(handle, CURLOPT_FOLLOWLOCATION, 1L);
+    }
     curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, write_data);
 
     if (fp){
@@ -54,13 +57,14 @@ void download(std::string url, std::string output){
     curl_global_cleanup();
 }
 
-void CurseForgeFix(std::string splittedline[]){
+void CurseForgeFix(std::string splittedline[], bool re){
+    splittedline[0] = splittedline[0].substr((re)?6:4);
     std::string p1 = splittedline[0].substr(0, splittedline[0].length() - 3);
     std::string p2 = splittedline[0].substr(splittedline[0].length() - 3).substr(0, 1).erase(1,1)+ splittedline[0].substr(splittedline[0].length() - 3).substr(1, 3);
     std::string fixedurl = "https://media.forgecdn.net/files/" + p1 + '/' + p2 + '/' + splittedline[1];
     std::cout << "Mod Name: " << splittedline[1] << '\n';
     std::cout << "Download Link: " << fixedurl << '\n';
-    download(fixedurl, curdir+'/'+splittedline[1]);
+    download(fixedurl, splittedline[1], re);
 }
 
 void ProcessReport(){
@@ -74,24 +78,37 @@ void ProcessReport(){
 }
 
 void Interpret(){
+    std::fstream metafile(modpackname+'/'+".metadata");
+
     if (line.substr(0, 5) == "name:"){
         std::cout << "--------------------------------------------\n";
         std::cout << "Modpack Name: " << line.substr(5) << '\n'; 
         std::cout << "--------------------------------------------\n";
         modpackname = line.substr(5);
         makedir(modpackname);
+    } else if(line.substr(0, 4) == "ver:"){
+        std::cout << "Version: " << line.substr(4) << '\n';
+        metafile << "ver: " << line.substr(4) << '\n';
     } else if (line[0] == '$'){
         curdir = modpackname+'/'+line.substr(1);
         makedir(curdir);
         dircount++;
     } else if (line[0] == '>'){
         dlname = line.substr(1);
-    } else if (line.substr(0, 4) == "http"){
-        download(line, dlname);
+    } else if(line.substr(0, 3) == "dl!"){
+        download (line.substr(3), dlname, false);
+    } else if (line.substr(0, 3) == "re!"){
+        download(line.substr(3), dlname, true);
         dlcount++;
-    } else if (line[0] == '!'){
+    } else if (line.substr(0,4) == "cfm!"){
         std::string arr[] = {split(line, "/").at(0), split(line, "/").at(1)};
-        CurseForgeFix(arr);
+        CurseForgeFix(arr, false);
+        dlcount++;
+    } else if (line.substr(0,6) == "recfm!"){
+        std::string arr[] = {split(line, "/").at(0), split(line, "/").at(1)};
+        CurseForgeFix(arr, true);
         dlcount++;
     }
+
+    metafile.close();
 }
